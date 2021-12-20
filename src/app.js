8f2f876b79c82320cea9811ca9aa69ad364c522e
Fatih Kaya <fatihkaya13@gmail.com>
2021-12-20 22:36:24 +0300

    add error handler middlewares to app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const helmet = require('helmet');
 const config = require('./config');
 const loaders = require('./loaders');
 const events = require('./scripts/events');
+const errorHandler = require('./middlewares/errorHandler');
+
 const {
     BlogsRoutes,
     CommentsRoutes,
@@ -19,10 +21,19 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 
+app.use('/blogs', BlogsRoutes);
+app.use('/comments', CommentsRoutes);
+app.use('/users', UsersRoutes);
+app.use('/readinglists', ReadinglistsRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Endpoint is not available.');
+    error.status = 404;
+    next(error);
+});
+
+app.use(errorHandler);
+
 app.listen(process.env.EXPRESS_APP_PORT, () => {
     console.log(`Server is running on port ${process.env.EXPRESS_APP_PORT}...`);
-    app.use('/blogs', BlogsRoutes);
-    app.use('/comments', CommentsRoutes);
-    app.use('/users', UsersRoutes);
-    app.use('/readinglists', ReadinglistsRoutes);
 });
